ce2a2daccd0cfd72592dd61dfbd4c0dc12d16229
faluyi faramoluwa <faluyi94@gmail.com>
2022-10-11 14:13:17 +0100

    updated

---
+++ src/component/calc-container/calc-container.styles.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const CalcContainerWrapper = styled.div`
+  width: 340px;
+  height: 540px;
+  padding: 10px;
+  border-radius: 10px;
+  background-color: #485461;
+  background-image: linear-gradient(315deg, #485461 0%, #28313b 74%);
+`;
